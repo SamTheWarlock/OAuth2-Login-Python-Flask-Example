9d203d2b7dfe6928821bf83d0e7607b9a69cc1e1
SamTheWarlock <43090976+SamTheWarlock@users.noreply.github.com>
2020-07-03 17:57:14 +0100

    Update discord_oauth_login_server.py

--- discord_oauth_login_server.py
+++ discord_oauth_login_server.py
@@ -68,7 +68,7 @@ def profile():
 
 
 # Or run like this
-# FLASK_APP=discord_oauth_login_server.py flask run -h 0.0.0.0 -p 8000
+# FLASK_APP=discord_oauth_login_server.py flask run -h 0.0.0.0 -p 8000g
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
 
